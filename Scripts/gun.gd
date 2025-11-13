4e5ed32198e9eb020084004a2cb2ba44a2a43ae4
taaavi1 <tvv@tlu.ee>
2025-11-13 21:28:14 +0200

    shoot where mouse

--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -1,19 +1,19 @@
 extends Area2D
 
+@onready var shooting_point = %ShootingPoint
+const BULLET = preload("res://Prefabs/bullet_2d.tscn")
 
 func _process(_delta):
-	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range.size() > 0:
-		var target_enemy = enemies_in_range.front()
-		look_at(target_enemy.global_position)
+	# Rotate to face the mouse position
+	var mouse_pos = get_global_mouse_position()
+	look_at(mouse_pos)
+
+	# If left mouse button is pressed, shoot
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 
 func shoot():
-	const BULLET = preload("res://Prefabs/bullet_2d.tscn")
 	var new_bullet = BULLET.instantiate()
-	new_bullet.global_transform = %ShootingPoint.global_transform
-	%ShootingPoint.add_child(new_bullet)
-
-
-func _on_timer_timeout() -> void:
-	shoot()
+	new_bullet.global_transform = shooting_point.global_transform
+	get_tree().current_scene.add_child(new_bullet)
