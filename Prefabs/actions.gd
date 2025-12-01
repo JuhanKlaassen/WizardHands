34ad1b2b547208726d8d8ae0865319b150cbc482
Juhan Klaassen <juhan04@tlu.ee>
2025-12-01 13:58:10 +0200

    F6 fix

--- Prefabs/actions.gd
+++ Prefabs/actions.gd
@@ -13,17 +13,17 @@ func _process(delta: float):
 func _input(event):
 	#DEBUG
 	# this is only run in editor, not in exported build
-	if OS.has_feature("editor"):
+	#if OS.has_feature("editor"):
 
-		if event.is_action_pressed("dbg_reload_game"):
-			#get_tree().call_deferred("reload_current_scene")
-			var items_to_delete: Array[Node] = get_tree().get_nodes_in_group("ground_items")
-			print("Found %d ground items to delete." % items_to_delete.size())
-			for item in items_to_delete:
-				# Use queue_free() to safely remove the node from memory 
-				# at the end of the current frame.
-				item.queue_free()
-			get_tree().reload_current_scene()
+	if event.is_action_pressed("dbg_reload_game"):
+		#get_tree().call_deferred("reload_current_scene")
+		var items_to_delete: Array[Node] = get_tree().get_nodes_in_group("ground_items")
+		print("Found %d ground items to delete." % items_to_delete.size())
+		for item in items_to_delete:
+			# Use queue_free() to safely remove the node from memory 
+			# at the end of the current frame.
+			item.queue_free()
+		get_tree().reload_current_scene()
 			
 		if event.is_action_pressed("dbg_end_game"):
 			get_tree().quit()
