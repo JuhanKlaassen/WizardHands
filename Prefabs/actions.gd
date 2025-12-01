84d5fcff2dff93aeb2e89e167da9401f877e5751
Juhan Klaassen <juhan04@tlu.ee>
2025-12-01 13:03:31 +0200

    GameOver reset pole vajutatav..

--- Prefabs/actions.gd
+++ Prefabs/actions.gd
@@ -23,9 +23,19 @@ func _input(event):
 				# Use queue_free() to safely remove the node from memory 
 				# at the end of the current frame.
 				item.queue_free()
-				get_tree().reload_current_scene()
+			get_tree().reload_current_scene()
 			
 		if event.is_action_pressed("dbg_end_game"):
 			get_tree().quit()
 	else:
 		pass
+
+
+func _on_link_button_pressed() -> void:
+	var items_to_delete: Array[Node] = get_tree().get_nodes_in_group("ground_items")
+	print("Found %d ground items to delete." % items_to_delete.size())
+	for item in items_to_delete:
+		# Use queue_free() to safely remove the node from memory 
+		# at the end of the current frame.
+		item.queue_free()
+	get_tree().reload_current_scene()
